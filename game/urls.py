c5d98ccb9b1a0a71f9c07de1370d6226959e6bf2
joshua clark <joshua.clark@tee2.org>
2019-05-11 16:19:20 -0700

    routing between pages

--- game/urls.py
+++ game/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'game'
 urlpatterns = [
 	path('', views.home, name="home"),
-	path('play/', views.game, name="game")
+	path('play/', views.game, name="game"),
+	path('scores/', views.scores, name="scores"),
+	path('profile/', views.profile, name="profile"),
 ]
--- game/views.py
+++ game/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.views import generic
 
 # Create your views here.
+def home(request):
+	return render(request, 'game/index.html')
+
 def game(request):
 	return render(request, 'game/game.html')
 
-def home(request):
-	return render(request, 'game/index.html')
+def scores(request):
+	return render(request, 'game/scores.html')
+
+def profile(request):
+	return render(request, 'game/profile.html')
+
+
 		
