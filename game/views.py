61d7723d3997d879f40554e69acbb3cc5f46d72b
zeebot <jjclark@attain.com>
2019-05-20 18:16:14 -0700

    added calculate score function

--- game/views.py
+++ game/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
 from game.models import Category, CurrentCategory, CurrentWord, SavedWord, CurrentScore
 
+# index view
 def index(request):
 	if request.method == 'POST':
 		current_category = CurrentCategory.objects.get()
@@ -13,13 +14,12 @@ def index(request):
 		category_list = Category.objects.all()
 		return render(request, 'game/index.html', {'category_list': category_list})
 
+# game view
 def game(request):
 	if request.method == 'POST':
 		current_category = CurrentCategory.objects.get()
 		saved_words = SavedWord.objects.all()
-		current_score = CurrentScore.objects.get()
-		current_score.current_score_text = current_score.current_score_text + 100
-		current_score.save()
+		current_score = calculateScore(100)
 		if CurrentWord.objects.get():
 			current_word = CurrentWord.objects.get()
 			current_word.current_word_text = request.POST['current_word']
@@ -34,31 +34,49 @@ def game(request):
 		'current_word': current_word.current_word_text, 
 		'saved_words': saved_words,
 		'letter_guess': current_word.current_word_text[-1].upper(),
-		'current_score': current_score.current_score_text
+		'current_score': current_score
 		}
 		return render(request, 'game/game.html', args)
 	if request.GET.get('clear_saved_words'):
 		current_word = CurrentWord.objects.filter(pk=1)
 		current_category = CurrentCategory.objects.get(id=1)
 		SavedWord.objects.all().delete()
-		current_score = CurrentScore.objects.get()
-		current_score.current_score_text = 0
-		current_score.save()
+		current_score = calculateScore(0)
 		args = {
 		'category': current_category.current_category_text,
-		'current_score': current_score.current_score_text
+		'current_score': current_score
 		}
 		return render(request, 'game/game.html', args)
 	else:
 		current_category = CurrentCategory.objects.get(id=1)
 		return render(request, 'game/game.html', {'category': current_category.current_category_text})
 
+# game view functions
+
+# The calculateScore function calculates the users score during gameplay
+# @params score  The amount at which the score will increase
+def calculateScore(score):
+	current_score = CurrentScore.objects.get()
+	# If score is zero reset the score to zero
+	if score == 0:
+		current_score.current_score_text = 0
+	# Add the score paramenter to the current score
+	else:
+		current_score.current_score_text = current_score.current_score_text + score
+	# Save the current score to the database
+	current_score.save()
+	# Return the current score
+	return current_score.current_score_text
+
+
+# instructions view
 def instructions(request):
 	return render(request, 'game/instructions.html')
 
+# scores view
 def scores(request):
 	return render(request, 'game/scores.html')
-
+# profile view
 def profile(request):
 	return render(request, 'game/profile.html')
 
