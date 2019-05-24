1639e73fd25c74b73b11678e9cea2f78ebc79cea
zeebot <jjclark@attain.com>
2019-05-23 21:39:00 -0700

    fixed whitenoise issues

--- catWords/settings.py
+++ catWords/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xh#fo7geu&*a&ms-nuchm-wdk!0kb7+ld7@t--)w0wjrf^)x7m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['category-words.herokuapp.com/']
+ALLOWED_HOSTS = ['category-words.herokuapp.com/','localhost']
 
 
 # Application definition
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # update database configuration 
 import dj_database_url 
